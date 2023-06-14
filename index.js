91e54ae0e5ca7298dcb3175e79a9f6470ec65ec1
gwynbleiddrivia <yasinzme2@gmail.com>
2023-06-14 16:09:51 +0600

    jwt verify middleware added

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const app = express()
 const port = process.env.PORT||5000
 const jwt = require('jsonwebtoken');
 
-
+//all middlwares
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
+const verifyJWT = (req, res, next) => {
+	const authorization = req.headers.authorization
+	if(!authorization){
+		return res.status(401).send({error: true, message: 'unauthorized access'})
+	}
+
+	const token = authorization.split(' ')[1]
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+		if(err){
+			return res.status(401).send({error:true, message:'unauthorized access'})
+		}
+		req.decoded = decoded
+		next()
+	})
+} 
+
 
 app.get('/',(req,res)=>{
 	res.send('nindo fusion camp server is running')
